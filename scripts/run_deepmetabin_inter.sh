a4cb9ff72b395f98a1c9974509b59b1802bcf44a
Zhenmiao Zhang <zhz142@ucsd.edu>
2024-03-13 17:50:58 +0800

    initial integration with script for interleaved

--- scripts/run_deepmetabin_inter.sh
+++ scripts/run_deepmetabin_inter.sh
@@ -24,10 +24,10 @@ then
 fi
 
 source ~/.bashrc; conda deactivate; conda activate deepmetabin
-python /datahome/datasets/ericteam/csgyguo/DeepMetaBin/preprocessing.py --outdir ./ --fasta contigs.fasta --bamfiles contigs.map.sorted.bam -m 1000
+python /datahome/datasets/ericteam/csgyguo/DeepMetaBin/preprocessing.py --outdir ./ --fasta /datahome/datasets/ericteam/csgyguo/DeepMetaBin/deepmetabin_out/contigs.fasta --bamfiles /datahome/datasets/ericteam/csgyguo/DeepMetaBin/deepmetabin_out/contigs.map.sorted.bam -m 1000
 #python /datahome/datasets/ericteam/zmzhang/csmxrao/DeepMetaBin/mingxing/deepmetabin/run.py datamodule.zarr_dataset_path=data.zarr datamodule.output=deepmetabin_out model.contignames_path=contignames.npz model.contig_path=contigs.fasta
-python /datahome/datasets/ericteam/csgyguo/DeepMetaBin/train.py -data data.zarr --contignames_path contignames.npz --contig_path contigs.fasta --output deepmetabin_out
+python /datahome/datasets/ericteam/csgyguo/DeepMetaBin/train.py -data data.zarr --contignames_path contignames.npz --contig_path /datahome/datasets/ericteam/csgyguo/DeepMetaBin/deepmetabin_out/contigs.fasta --output deepmetabin_out
 
 cd deepmetabin_out/results/pre_bins && checkm lineage_wf -t 100 -x fasta --tab_table -f checkm.tsv ./ ./
 cd ../../../
-python /datahome/datasets/ericteam/csgyguo/deepmetabin/secondary_clustering.py --primary_out deepmetabin_out --contigname_path contignames.npz --output_path deepmetabin_out/results --binned_length 1000 --contig_path contigs.fasta
+python /datahome/datasets/ericteam/csgyguo/DeepMetaBin/secondary_clustering.py --primary_out deepmetabin_out --contigname_path contignames.npz --output_path deepmetabin_out/results --binned_length 1000 --contig_path /datahome/datasets/ericteam/csgyguo/DeepMetaBin/deepmetabin_out/contigs.fasta
